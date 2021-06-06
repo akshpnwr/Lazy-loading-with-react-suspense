63c7169776d672df01b76b04c88372ade0ca1ed6
akshpnwr <akashpanwar9976@gmail.com>
2021-06-06 21:08:27 +0530

    closure and useCallback dependencies

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import "./App.css";
 function App() {
   const [showPara, setShowPara] = useState(false);
 
+  const [allowToggle, setAllowToggle] = useState(false);
+
   console.log("APP RUNNING");
 
   const toggleHandler = useCallback(() => {
-    setShowPara((prevShowPara) => !prevShowPara);
-  }, []);
+    console.log(allowToggle);
+    if (allowToggle) {
+      setShowPara((prevShowPara) => !prevShowPara);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <Demo show={showPara} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleHandler}>Toggle</Button>
     </div>
   );
